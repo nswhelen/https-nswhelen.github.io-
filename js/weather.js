9205d78a2828889c8f51e13cd59912495f4e39ba
sunwoonam <swoonam1004@gmail.com>
2024-10-20 23:22:33 +1000

    Initial commit

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "ce4101fd8fd9550ed3d1877c8c95e9f6";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
